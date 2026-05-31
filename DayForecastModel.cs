bc3400860b340e515a1d3d16e79297f27442acd8
Irina-enotyk <Kolskaya2016@gmail.com>
2026-05-31 21:56:26 +0300

    Add project files.

---
+++ DayForecastModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherWpfApp
+{
+    public class DayForecastModel
+    {
+        public DateTime Date { get; set; }
+        public float MaxTemperature { get; set; }
+        public float MinTemperature { get; set; }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WeatherWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var data = new DayForecastModel
+            {
+                Date = DateTime.Now,
+                MaxTemperature = 20,
+                MinTemperature = 10
+            };
+            var data2 = new DayForecastModel
+            {
+                Date = DateTime.Now,
+                MaxTemperature = 25,
+                MinTemperature = 14
+            };
+
+            WeatherDays_ListBox.ItemsSource = new List<DayForecastModel>
+            {
+                data, data2, data, data2
+            };                                                                      
+        }
+
+        private void WeatherDayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button button)
+            {
+                var day = button.DataContext as DayForecastModel;
+                Details_StackPanel.DataContext = day;
+            }
+        }
+    }
+}
