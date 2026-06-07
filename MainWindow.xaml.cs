be322618dd89770e28122ff9b5f1c719a01415e1
Irina-enotyk <Kolskaya2016@gmail.com>
2026-06-07 10:27:05 +0300

    Reapply "Сделала мамкет почасового прогноза"
    This reverts commit 0df21d627fc27e704e1b3310f119708e85fa2b8f.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -96,7 +96,7 @@ namespace WeatherWpfApp
 
         private void LoadForecastData()
         {
-            WeatherDays_ListBox.ItemsSource = ForecastData.Load();
+            WeatherDays_ListBox.ItemsSource = DayForecastData.Load();
         }
     }
 }
