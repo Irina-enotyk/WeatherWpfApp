aa1dcbf57a241b79ee2cb554db7446f8577242bb
Irina-enotyk <Kolskaya2016@gmail.com>
2026-06-12 09:34:14 +0300

    Не вижу, почему не срабатывает загрузка данных в ленту при нажатии Home

--- ViewModels/HomeViewViewModel.cs
+++ ViewModels/HomeViewViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WeatherWpfApp.Models;
+using WeatherWpfApp.Storages;
 
 namespace WeatherWpfApp.ViewModels
 {
@@ -6,5 +9,28 @@ namespace WeatherWpfApp.ViewModels
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        public List<DayForecastModel> ForecastDays
+        {
+            get => forecastDays;
+            set
+            {
+                forecastDays = value;
+                OnPropertyChanged();
+            }
+                
+        }
+
+        public HomeViewViewModel()
+        {
+            ForecastDays = WeatherDataStorage.Load();
+        }
+
+        private List<DayForecastModel> forecastDays;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
