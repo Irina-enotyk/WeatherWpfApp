e0cef601bf7efcc211066acb037bad6a308663c2
Irina-enotyk <Kolskaya2016@gmail.com>
2026-06-19 04:17:22 +0300

    Настроила отображение информации в окне детального прогноза

--- Views/Home/DayForecastDetailsView.xaml.cs
+++ Views/Home/DayForecastDetailsView.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WeatherWpfApp.Views.Home
 {
     /// <summary>
     /// Interaction logic for DayForecastDetailsView.xaml
     /// </summary>
-    public partial class DayForecastDetailsView : Window
+    public partial class DayForecastDetailsView : UserControl
     {
         public DayForecastDetailsView()
         {
