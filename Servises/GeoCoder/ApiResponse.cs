541b72c59e0f804127480faa18ec27e1c9860efa
Irina-enotyk <Kolskaya2016@gmail.com>
2026-07-26 11:19:43 +0300

    Сделала запрос координат по названию города и записала в свойства класса ApiResponse данные из полученного ответа.

---
+++ Servises/GeoCoder/ApiResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace WeatherWpfApp.Servises.GeoCoder
+{
+    public class ApiResponse
+    {
+        public Response Response { get; set; }
+    }
+
+    public class Response
+    {
+        public GeoObjectCollection GeoObjectCollection { get; set; }
+    }
+
+    public class GeoObjectCollection
+    {
+        public FeatureMember[] FeatureMember { get; set; }
+    }
+
+    public class FeatureMember
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Point Point { get; set; }
+    }
+
+    public class Point
+    {
+        public string Pos { get; set; }
+    }
+}
---
+++ Servises/GeoCoder/GeoCoderService.cs
@@ -0,0 +1,58 @@
+﻿
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace WeatherWpfApp.Servises.GeoCoder
+{
+    public class GeoCoderService
+    {
+        private readonly string apikey = LoadApiKey();
+        private readonly HttpClient httpClient = new HttpClient();
+
+        private static string LoadApiKey()
+        {
+            var path = Path.Combine(AppContext.BaseDirectory, "apikey.txt");
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException(
+                    "Не найден файл apikey.txt с ключом API Яндекс Геокодера. " +
+                    "Скопируйте apikey.txt.sample в apikey.txt и вставьте свой ключ.",
+                    path);
+            }
+
+            return File.ReadAllText(path).Trim();
+        }
+
+        public List<GeoLocation> GetLocations (string place)
+        {
+            var url = $"https://geocode-maps.yandex.ru/1.x/?apikey={apikey}&geocode={place}&format=json";
+
+            var response = httpClient.GetFromJsonAsync<ApiResponse>(url).Result;
+
+            return ToGeoLocation(response);
+        }
+
+        private List<GeoLocation> ToGeoLocation(ApiResponse? response)
+        {
+            var locations = new List<GeoLocation>();
+
+            foreach (var item in response.Response.GeoObjectCollection.FeatureMember)
+            {
+                var location = new GeoLocation();
+
+                location.Name = item.Name;
+                location.Description = item.Description;
+
+                var points = item.Point.Pos.Split(" ");
+
+                location.Longitude = Convert.ToDouble(points[0]);
+                location.Latitude = Convert.ToDouble(points[1]);
+
+                locations.Add(location);
+            }
+            return locations;
+        }
+    }
+}
